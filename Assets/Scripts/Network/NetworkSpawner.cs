0802be44085f5b57f219249fccaf9ac13abcb197
Diabolica Elias <scripter.diabolica@gmail.com>
2024-12-29 19:58:50 +0200

    Added follow camera

--- Assets/Scripts/Network/NetworkSpawner.cs
+++ Assets/Scripts/Network/NetworkSpawner.cs
@@ -1,11 +1,22 @@
 using Fusion;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class NetworkSpawner : MonoBehaviour
 {
     [SerializeField] private NetworkPrefabRef playerPrefab;
     private Dictionary<PlayerRef, NetworkObject> spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+    private CinemachineCamera virtualCamera;
+
+    private void Start()
+    {
+        virtualCamera = FindFirstObjectByType<CinemachineCamera>();
+        if (virtualCamera == null)
+        {
+            Debug.LogWarning("CinemachineCamera not found in the scene.");
+        }
+    }
 
     public void HandlePlayerJoined(NetworkRunner runner, PlayerRef player)
     {
@@ -14,6 +25,11 @@ public class NetworkSpawner : MonoBehaviour
             Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 1, 0);
             NetworkObject networkPlayerObject = runner.Spawn(playerPrefab, spawnPosition, Quaternion.identity, player);
             spawnedCharacters.Add(player, networkPlayerObject);
+
+            if (networkPlayerObject.HasInputAuthority && virtualCamera != null)
+            {
+                virtualCamera.Follow = networkPlayerObject.transform;
+            }
         }
     }
 
