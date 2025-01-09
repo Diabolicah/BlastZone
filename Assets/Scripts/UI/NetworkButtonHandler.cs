854e44a5e4a576cf00307b8ce070bc0be15dec2a
Ori Hytner <hytnerori@gmail.com>
2025-01-09 22:11:26 +0200

    Added Host,Join buttons to Multiplayer screen

---
+++ Assets/Scripts/UI/NetworkButtonHandler.cs
@@ -0,0 +1,43 @@
+using Fusion;
+using UnityEngine;
+using static UI.MainMenu_Logic;
+namespace UI
+{
+    public class NetworkButtonHandler : MonoBehaviour
+    {
+        private NetworkManager _networkManager; // Reference to the Photon Fusion NetworkManager
+        
+        private void Start()
+        {
+            _networkManager = FindFirstObjectByType<NetworkManager>();
+        }
+        
+        public void HostGame()
+        {
+            if (_networkManager != null)
+            {
+                _networkManager.StartGame(GameMode.Host);
+                unityObjects["Screen_Multiplayer"].gameObject.SetActive(false);
+                unityObjects["Img_Background"].gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("NetworkManager is not assigned!");
+            }
+        }
+        
+        public void JoinGame()
+        {
+            if (_networkManager != null)
+            {
+                _networkManager.StartGame(GameMode.Client);
+                unityObjects["Screen_Multiplayer"].gameObject.SetActive(false);
+                unityObjects["Img_Background"].gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("NetworkManager is not assigned!");
+            }
+        }
+    }
+}
