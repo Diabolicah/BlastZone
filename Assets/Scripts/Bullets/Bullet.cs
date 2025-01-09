200991e51e59214424518ce48f8de82d51ab812e
julio1703 <julio.elias22@gmail.com>
2025-01-09 23:40:48 +0200

    added fixedupdatenetwork and initialize to bullet script

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -3,15 +3,29 @@ using UnityEngine;
 
 public class Bullet : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Networked] public Vector3 _direction { get; set; } // Networked bullet direction
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _lifeTime = 5f;
+    [SerializeField] private TickTimer _life;
+
+    public void Initialize(Vector3 direction)
     {
-        
+        _direction = direction;
+        _life = TickTimer.CreateFromSeconds(Runner, _lifeTime);
     }
 
-    // Update is called once per frame
-    void FixedUpdateNetwork()
+
+    // FixedUpdateNetwork is called once per server tick
+    public override void FixedUpdateNetwork()
     {
-        
+        if (Runner.IsForward)
+        {
+            transform.position += _direction * _speed * Time.deltaTime;
+            if (_life.ExpiredOrNotRunning(Runner))
+            {
+                Runner.Despawn(Object); // Despawn the bullet after its lifetime expires
+            }
+        }
+
     }
 }
