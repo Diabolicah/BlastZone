d59bac8e378c79e345e0fe613d9dfa73b7230b87
Ori Hytner <hytnerori@gmail.com>
2025-02-05 00:54:29 +0200

    added color and intractable features to game modes buttons

--- Assets/Scripts/UI/NetworkButtonHandler.cs
+++ Assets/Scripts/UI/NetworkButtonHandler.cs
@@ -1,14 +1,27 @@
 using Fusion;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 using static UI.MainMenu_Logic;
 namespace UI
 {
     public class NetworkButtonHandler : MonoBehaviour
     {
         private NetworkManager _networkManager; // Reference to the Photon Fusion NetworkManager
-        
+        private TextMeshProUGUI loadingText;
+        private GameObject DeathmatchButton;
+        private TextMeshProUGUI DeathmatchText;
+        private GameObject TeamDeathmatchButton;
+        private TextMeshProUGUI TeamDeathmatchText;
+
         private void Start()
         {
+            loadingText = GameObject.Find("Txt_Loading").GetComponent<TextMeshProUGUI>();
+            DeathmatchButton = GameObject.Find("Btn_Deathmatch");
+            TeamDeathmatchButton = GameObject.Find("Btn_TeamDeathmatch");
+            DeathmatchText = DeathmatchButton.GetComponentInChildren<TextMeshProUGUI>();
+            TeamDeathmatchText = TeamDeathmatchButton.GetComponentInChildren<TextMeshProUGUI>();
             _networkManager = FindFirstObjectByType<NetworkManager>();
         }
         
@@ -16,7 +29,10 @@ namespace UI
         {
             if (_networkManager != null)
             {
-                bool success = await _networkManager.StartMatchmaking("Test");
+                DeathmatchButton.GetComponent<Button>().interactable = false;
+                DeathmatchText.color = Color.gray;
+                loadingText.text = "Loading Deathmatch ...";
+                bool success = await _networkManager.StartMatchmaking("Deathmatch");
                 if (success)
                 {
                     unityObjects["Screen_Multiplayer"].gameObject.SetActive(false);
@@ -25,7 +41,10 @@ namespace UI
                 }
                 else
                 {
-
+                    DeathmatchButton.GetComponent<Button>().interactable = true;
+                    DeathmatchText.color = Color.white;
+                    loadingText.text = "Failed to connect to deathmatch";
+                    Debug.LogError("Failed to start matchmaking for Deathmatch");
                 }
 
             }
@@ -39,10 +58,23 @@ namespace UI
         {
             if (_networkManager != null)
             {
-                await _networkManager.StartMatchmaking("TeamDeathmatch");
-                unityObjects["Screen_Multiplayer"].gameObject.SetActive(false);
-                unityObjects["Img_Background"].gameObject.SetActive(false);
-                unityObjects["Screen_Game"].gameObject.SetActive(true);
+                TeamDeathmatchButton.GetComponent<Button>().interactable = false;
+                TeamDeathmatchText.color = Color.gray;
+                loadingText.text = "Loading Team Deathmatch ...";
+                bool success = await _networkManager.StartMatchmaking("TeamDeathmatch");
+                if(success)
+                {
+                    unityObjects["Screen_Multiplayer"].gameObject.SetActive(false);
+                    unityObjects["Img_Background"].gameObject.SetActive(false);
+                    unityObjects["Screen_Game"].gameObject.SetActive(true);
+                }
+                else
+                {
+                    TeamDeathmatchButton.GetComponent<Button>().interactable = true;
+                    TeamDeathmatchText.color = Color.white;
+                    loadingText.text = "Failed to connect to team deathmatch";
+                    Debug.LogError("Failed to start matchmaking for Team Deathmatch");
+                }
             }
             else
             {
