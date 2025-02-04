b2677dc414122997289b397749a2692a9c7421d5
Diabolica Elias <scripter.diabolica@gmail.com>
2025-02-04 02:08:09 +0200

    Added scripts to the player prefab and minor adjustments to the PlayerScript

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,9 +5,11 @@ public class Player : NetworkBehaviour
 {
     private NetworkCharacterController _characterController;
     
-    private void Awake()
+    private StatsManager _statsManager;
+    public override void Spawned()
     {
         _characterController = GetComponent<NetworkCharacterController>();
+        _statsManager = GetComponent<StatsManager>();
     }
 
     public override void FixedUpdateNetwork()
