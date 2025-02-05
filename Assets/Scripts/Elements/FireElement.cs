78dc7a0c0d13fc6f3910d1ee467e7b97f78343d3
julio1703 <julio.elias22@gmail.com>
2025-02-05 16:38:09 +0200

    added range to the firedamagepercentagefrombulletdamage in the fireelement script

--- Assets/Scripts/Elements/FireElement.cs
+++ Assets/Scripts/Elements/FireElement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class FireElement : Element
 {
     [SerializeField] private float _fireDamageDuration = 5f;
+    [Range(0.1f, 1f)]
     [SerializeField] private float _fireDamagePercentageFromBulletDamage = 0.2f;
 
     public override void ElementEffect(NetworkObject Shooter, NetworkObject target)
