7fd74acb63a0357a20568009f1897775ed01d9cf
julio1703 <julio.elias22@gmail.com>
2025-02-05 16:02:03 +0200

    Update FireElement.cs

--- Assets/Scripts/Elements/FireElement.cs
+++ Assets/Scripts/Elements/FireElement.cs
@@ -1,7 +1,14 @@
+using Fusion;
 using UnityEngine;
 
 public class FireElement : Element
 {
     [SerializeField] private float _fireDamageDuration;
+    [SerializeField] private int _fireDamagePercentageFromBulletDamage;
 
+    public override void ElementEffect(NetworkObject Shooter, NetworkObject target)
+    {
+
+    }
 }
+    
