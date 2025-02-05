70a152bb222bb92199fedab279a8078b8b4ffda2
julio1703 <julio.elias22@gmail.com>
2025-02-05 17:14:49 +0200

    Update FireEffect.cs

--- Assets/Scripts/Elements/Effects/FireEffect.cs
+++ Assets/Scripts/Elements/Effects/FireEffect.cs
@@ -6,17 +6,30 @@ public class FireEffect : IElement
 {
     private float _fireDamage;
     private float _fireDamageDuration;
+    private float _durationBetweenFireTicks;
     private CooldownManager _fireDurationManager;
+    private CooldownManager _fireTickManager;
 
     public FireEffect(float fireDamage, float fireDamageDuration)
     {
         _fireDamage = fireDamage;
         _fireDamageDuration = fireDamageDuration;
+        _durationBetweenFireTicks = 1f;
         _fireDurationManager = new CooldownManager();
+        _fireTickManager = new CooldownManager();
+
     }
 
     public void activate(NetworkRunner runner, NetworkObject bulletShooter, Vector3 position, NetworkObject playersHit)
     {
-        Debug.Log(playersHit.ToString() + " Is On Fire");
+        _fireDurationManager.ResetCooldown(runner, _fireDamageDuration);
+        while (!_fireDurationManager.IsCooldownExpiredOrNutRunning(runner))
+        {
+            if(_fireTickManager.IsCooldownExpiredOrNutRunning(runner))
+            {
+                Debug.Log("Fire Tick");
+                _fireTickManager.ResetCooldown(runner, _durationBetweenFireTicks);
+            }
+        }
     }
 }
