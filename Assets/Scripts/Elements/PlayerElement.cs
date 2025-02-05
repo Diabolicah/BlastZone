37fbe5aaad5cb6444248f21d77599dec4f601e5e
julio1703 <julio.elias22@gmail.com>
2025-02-05 18:35:12 +0200

    Update PlayerElement.cs

--- Assets/Scripts/Elements/PlayerElement.cs
+++ Assets/Scripts/Elements/PlayerElement.cs
@@ -2,11 +2,42 @@ using System.Collections.Generic;
 using Fusion;
 using NUnit.Framework;
 using UnityEngine;
+using static UnityEditor.Rendering.FilterWindow;
 
 public class PlayerElement : NetworkBehaviour
 {
-  public void useElement(IElement element, NetworkObject elementShooter, Vector3 usedElementPosition, NetworkObject playerHits)
-   {
-        element.activate(Runner, elementShooter, usedElementPosition, playerHits);
-   }
+    private bool _isElementUsed = false;
+    private IElement _element;
+    private NetworkObject _elementShooter;
+    private Vector3 _usedElementPosition;
+    private NetworkObject _playerHit;
+    private CooldownManager _elementDurationManager;
+
+    public override void Spawned()
+    {
+        _elementDurationManager = new CooldownManager();
+    }
+    public override void FixedUpdateNetwork()
+    {
+        if (_elementDurationManager.IsCooldownExpiredOrNutRunning(Runner))
+        {
+            _isElementUsed = false;
+        }
+
+        if (_isElementUsed)
+        {
+            _element.activate(Runner, _elementShooter, _usedElementPosition, _playerHit);
+        }
+    }
+
+
+    public void useElement(IElement element, float elementDuration,NetworkObject elementShooter, Vector3 usedElementPosition, NetworkObject playerHit)
+    {
+        _element = element;
+        _elementShooter = elementShooter;
+        _usedElementPosition = usedElementPosition;
+        _playerHit = playerHit;
+        _isElementUsed = true;
+        _elementDurationManager.ResetCooldown(Runner, elementDuration);
+    }
 }
