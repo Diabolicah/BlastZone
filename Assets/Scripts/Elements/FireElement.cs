a5279d278273a9e06860e3ab1ab0d33958d777d4
julio1703 <julio.elias22@gmail.com>
2025-02-05 16:09:21 +0200

    Update FireElement.cs

--- Assets/Scripts/Elements/FireElement.cs
+++ Assets/Scripts/Elements/FireElement.cs
@@ -8,7 +8,11 @@ public class FireElement : Element
 
     public override void ElementEffect(NetworkObject Shooter, NetworkObject target)
     {
-
+        if (Shooter.TryGetComponent<PlayerElement>(out PlayerElement playerElement))
+        {
+            float fireDamage = BulletDamage * _fireDamagePercentageFromBulletDamage;
+            playerElement.useElement(new FireEffect(BulletDamage, _fireDamageDuration), Shooter, transform.position, target);
+        }
     }
 }
     
