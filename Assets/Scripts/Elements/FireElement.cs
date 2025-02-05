fa9b7023aca24be59c8b25e87c1124ffff70244a
julio1703 <julio.elias22@gmail.com>
2025-02-05 16:29:25 +0200

    Update FireElement.cs

--- Assets/Scripts/Elements/FireElement.cs
+++ Assets/Scripts/Elements/FireElement.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class FireElement : Element
 {
-    [SerializeField] private float _fireDamageDuration;
-    [SerializeField] private int _fireDamagePercentageFromBulletDamage;
+    [SerializeField] private float _fireDamageDuration = 5f;
+    [SerializeField] private float _fireDamagePercentageFromBulletDamage = 0.2f;
 
     public override void ElementEffect(NetworkObject Shooter, NetworkObject target)
     {
+        Debug.Log("ElementEffect");
         if (Shooter.TryGetComponent<PlayerElement>(out PlayerElement playerElement))
         {
             float fireDamage = BulletDamage * _fireDamagePercentageFromBulletDamage;
