56b44321aff02e3f9af3c6583215f940489f0654
Diabolica <scripter.Diabolica@gmail.com>
2025-03-07 19:41:54 +0200

    Fixes to Team Manager to work cross client

--- Assets/Scripts/Managers/SC_TeamManager.cs
+++ Assets/Scripts/Managers/SC_TeamManager.cs
@@ -1,26 +1,14 @@
+using Fusion;
 using UnityEngine;
 
-public class SC_TeamManager : MonoBehaviour
+public class SC_TeamManager : NetworkBehaviour
 {
-    // Singleton instance
-    private static SC_TeamManager _instance;
-    public static SC_TeamManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                // Optionally create a new GameObject to hold the manager if none is found
-                GameObject obj = new GameObject("TeamManager");
-                _instance = obj.AddComponent<SC_TeamManager>();
-                DontDestroyOnLoad(obj);
-            }
-            return _instance;
-        }
-    }
+    // Networked team counters, synchronized across all clients.
+    [Networked] private int _team1Count { get; set; }
+    [Networked] private int _team2Count { get; set; }
 
-    private int _team1Count;
-    private int _team2Count;
+    private static SC_TeamManager _instance;
+    public static SC_TeamManager Instance => _instance;
 
     private void Awake()
     {
@@ -33,30 +21,118 @@ public class SC_TeamManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    public override void Spawned()
+    {
+        if (Object.HasStateAuthority)
+        {
+            _team1Count = 0;
+            _team2Count = 0;
+        }
+    }
+
+    private void IncrementTeam1Count()
+    {
+        if (Object.HasStateAuthority)
+            _team1Count++;
+    }
+
+    private void DecrementTeam1Count()
+    {
+        if (Object.HasStateAuthority && _team1Count > 0)
+            _team1Count--;
+    }
+
+    private void IncrementTeam2Count()
+    {
+        if (Object.HasStateAuthority)
+            _team2Count++;
+    }
+
+    private void DecrementTeam2Count()
+    {
+        if (Object.HasStateAuthority && _team2Count > 0)
+            _team2Count--;
+    }
+
     public int GetTeam()
     {
+
+
         if (_team1Count > _team2Count)
         {
-            _team2Count++;
+            if (!Object.HasStateAuthority)
+            {
+                RPC_RequestIncrementTeam(2);
+            }
+            else
+            {
+                IncrementTeam2Count();
+            }
             return 2;
         }
         else
         {
-            _team1Count++;
+            if (!Object.HasStateAuthority)
+            {
+                RPC_RequestIncrementTeam(1);
+            }
+            else
+            {
+                IncrementTeam1Count();
+            }
             return 1;
         }
     }
-    public void RemovePlayer(int teamNumber)
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void RPC_RequestIncrementTeam(int teamNumber, RpcInfo info = default)
     {
-        if (teamNumber == 1 && _team1Count > 0)
+        if (!Object.HasStateAuthority)
+            return;
+
+        if (teamNumber == 1)
         {
-            _team1Count--;
+            IncrementTeam1Count();
         }
-        else if (teamNumber == 2 && _team2Count > 0)
+        else if (teamNumber == 2)
         {
-            _team2Count--;
+            IncrementTeam2Count();
         }
     }
+
+    public void RemovePlayer(int teamNumber)
+    {
+        if (!Object.HasStateAuthority)
+        {
+            RPC_RequestRemovePlayer(teamNumber);
+            return;
+        }
+
+        if (teamNumber == 1)
+        {
+            DecrementTeam1Count();
+        }
+        else if (teamNumber == 2)
+        {
+            DecrementTeam2Count();
+        }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    private void RPC_RequestRemovePlayer(int teamNumber, RpcInfo info = default)
+    {
+        if (!Object.HasStateAuthority) return;
+
+        if (teamNumber == 1)
+        {
+            DecrementTeam1Count();
+        }
+        else if (teamNumber == 2)
+        {
+            DecrementTeam2Count();
+        }
+    }
+
     public int Team1Count => _team1Count;
     public int Team2Count => _team2Count;
 }
